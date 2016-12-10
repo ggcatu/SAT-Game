3699a170e03884e64a1780593ec186ae806224e1
Antoxx9 <anthonyj.elk@gmail.com>
2016-12-09 21:10:00 -0430

    Clausulas tipo 2

--- pre_processor.cpp
+++ pre_processor.cpp
@@ -38,6 +38,7 @@ int clausula = 1;
 vector< vector<Celda> > Celdas;
 vector< vector<int> > ClausulasCero;
 vector< vector<int> > ClausulasUno;
+vector< vector<int> > ClausulasDos;
 
 void GenerarCero(int N, int M){
     int aux,aux2;
@@ -203,6 +204,124 @@ void GenerarUno(int N, int M){
     }
 }
 
+void GenerarDos(int N, int M){
+    vector<int> auxV;
+    int n,s,e,w,z;
+    for(int j = 0; j < M; j++){
+        auxV.clear();
+        n = Celdas[1][j].n;
+        s = Celdas[1][j].s;
+        e = Celdas[1][j].e;
+        w = Celdas[1][j].w;
+        z = Celdas[1][j].z;
+        auxV.push_back(w);
+        auxV.push_back(z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+        auxV.push_back(-w);
+        auxV.push_back(-z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+    }
+    for(int j = 0; j < M; j++){
+        auxV.clear();
+        n = Celdas[N-1][j].n;
+        s = Celdas[N-1][j].s;
+        e = Celdas[N-1][j].e;
+        w = Celdas[N-1][j].w;
+        z = Celdas[N-1][j].z;
+        auxV.push_back(e);
+        auxV.push_back(z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+        auxV.push_back(-e);
+        auxV.push_back(-z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+    }
+    for(int i = 0; i < N; i++){
+        auxV.clear();
+        n = Celdas[i][1].n;
+        s = Celdas[i][1].s;
+        e = Celdas[i][1].e;
+        w = Celdas[i][1].w;
+        z = Celdas[i][1].z;
+        auxV.push_back(s);
+        auxV.push_back(z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+        auxV.push_back(-s);
+        auxV.push_back(-z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+    }
+    for(int i = 0; i < N; i++){
+        auxV.clear();
+        n = Celdas[i][M-1].n;
+        s = Celdas[i][M-1].s;
+        e = Celdas[i][M-1].e;
+        w = Celdas[i][M-1].w;
+        z = Celdas[i][M-1].z;
+        auxV.push_back(n);
+        auxV.push_back(z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+        auxV.push_back(-n);
+        auxV.push_back(-z);
+        ClausulasDos.push_back(auxV);
+        auxV.clear();
+    }
+    for(int i = 1; i < N-1; i++){
+        for(int j = 1; j < M-1; j++){
+            auxV.clear();
+            n = Celdas[i][j].n;
+            s = Celdas[i][j].s;
+            e = Celdas[i][j].e;
+            w = Celdas[i][j].w;
+            z = Celdas[i][j].z;
+            auxV.push_back(z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(n);
+            auxV.push_back(-Celdas[i][j+1].z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(e);
+            auxV.push_back(-Celdas[i+1][j].z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(s);
+            auxV.push_back(-Celdas[i][j-1].z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(w);
+            auxV.push_back(-Celdas[i-1][j].z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(n);
+            auxV.push_back(-Celdas[i][j+1].z);
+            auxV.push_back(z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(e);
+            auxV.push_back(-Celdas[i+1][j].z);
+            auxV.push_back(z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(s);
+            auxV.push_back(-Celdas[i][j-1].z);
+            auxV.push_back(z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+            auxV.push_back(w);
+            auxV.push_back(-Celdas[i-1][j].z);
+            auxV.push_back(z);
+            ClausulasDos.push_back(auxV);
+            auxV.clear();
+        }
+    }
+}
+
 int main(int argc, char * argv[]){
 	int n, m, index;
     string line;
@@ -222,12 +341,28 @@ int main(int argc, char * argv[]){
         }
         cout << clausula << endl;
         cout << Celdas[0][1].n << endl;
+        cout << "Clausulas 0" << endl;
         GenerarCero(n,m);
         for(int i = 0; i < ClausulasCero.size(); i++){
             cout << "{" << ClausulasCero[i][0] << "v" << ClausulasCero[i][1] << "}" << " and " << endl;
         }
         cout << endl;
+        cout << "Clausulas 1" << endl;
         GenerarUno(n,m);
+        for(int i = 0; i < ClausulasUno.size(); i++){
+            cout << "{";
+            for(int j = 0; j < ClausulasUno[i].size(); j++){
+                if (j < ClausulasUno[i].size() -1){
+                    cout << ClausulasUno[i][j] << "v" ;
+                }
+                else{
+                    cout << ClausulasUno[i][j] << "}" ;
+                }
+            }
+            cout << "and" << endl;
+        }
+        cout << "Clausulas 2" << endl;
+        GenerarDos(n,m);
         for(int i = 0; i < ClausulasUno.size(); i++){
             cout << "{";
             for(int j = 0; j < ClausulasUno[i].size(); j++){
