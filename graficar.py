356f5b5f758927e93dcd556bf843525fc12a17c4
Antoxx9 <anthonyj.elk@gmail.com>
2016-12-10 22:43:01 -0430

    Cosas merge

--- graficar.py
+++ graficar.py
@@ -8,7 +8,7 @@ resultado = input("Coloque el output")
 '''
 tablero = "5 5 ..32. 222.3 0..1. 2.2.. .2323"
 
-resultado = "5 5 11011 101101 10101 010010 01101 000101 01001 011110 10001 101101 11111"
+resultado = "5 5 11011 101110 10101 010001 01101 000110 01101 010001 10101 101110 11011"
 
 tablero = tablero.split()
 resultado = resultado.split()
